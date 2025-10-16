6e460db20ef38e22cb14a61667119bc271f2a5ba
kavanap37 <kavanap12@gmail.com>
2025-10-17 01:04:37 +0530

    cors, axios, withcredentials

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,53 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("user1@gmail.com");
+  const [password, setPassword] = useState("User1@pass123");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className="flex justify-center my-5">
+      <div className=" card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <input
+            type="text"
+            placeholder="Email ID"
+            className="input"
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-200 shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">DevSocials</a>
       </div>
