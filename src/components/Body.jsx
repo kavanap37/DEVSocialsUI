596f97543c9d53619877f7628ec24a45c90af124
kavanap37 <kavanap12@gmail.com>
2025-12-10 08:11:14 +0530

    Authorizing routes, useEffect, Link to, store check before api call

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => {
+    store.data;
+  });
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res.data);
+      dispatch(addUser(res?.data));
+    } catch (e) {
+      if (e.status === 401) {
+        navigate("/login");
+      }
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <NavBar />
