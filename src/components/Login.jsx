59a79098cdea57eff0ef5a03711d2f609bf3383f
kavanap37 <kavanap12@gmail.com>
2026-01-08 10:12:21 +0530

    feat: SignUp in Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,11 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  const [emailId, setEmailId] = useState("user1@gmail.com");
-  const [password, setPassword] = useState("User1@pass123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("LNU");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -30,11 +33,49 @@ const Login = () => {
       setError(err?.response?.data || "Something went wrong");
     }
   };
+  const handleSignUp = async () => {
+    try {
+      // console.log(firstName, lastName, emailId);
+      // lastName = lastName || " ";
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      console.error(err);
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
   return (
     <div className="flex justify-center my-5">
       <div className=" card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          {!isLoginForm && (
+            <>
+              <input
+                type="text"
+                value={firstName}
+                className="input"
+                placeholder="First Name"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+
+              <input
+                type="text"
+                value={lastName}
+                className="input"
+                placeholder="Last Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </>
+          )}
           <input
             type="text"
             placeholder="Email ID"
@@ -51,10 +92,21 @@ const Login = () => {
           />
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
